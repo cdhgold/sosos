2dee8d9437c657a09f9a8ae26313ea4c50baca0f
cdhgold <cdhgold@naver.com>
2020-09-09 15:36:06 +0900

    carwash

---
+++ app/src/main/java/com/cdhgold/carwash/MainActivity.java
@@ -0,0 +1,120 @@
+package com.cdhgold.carwash;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+import com.google.android.play.core.tasks.Task;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppUpdateManager mAppUpdateManager; // auto update
+    private final int MY_REQUEST_CODE = 100;// auto update
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        // 메뉴
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_map)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+         /*
+        uto update check
+         */
+        mAppUpdateManager = AppUpdateManagerFactory.create(getApplicationContext());
+        // 업데이트 사용 가능 상태인지 체크
+        Task<AppUpdateInfo> appUpdateInfoTask = mAppUpdateManager.getAppUpdateInfo();
+        // 사용가능 체크 리스너를 달아준다
+        appUpdateInfoTask.addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
+            @Override
+            public void onSuccess(AppUpdateInfo appUpdateInfo) {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                        && // 유연한 업데이트 사용 시 (AppUpdateType.FLEXIBLE) 사용
+                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                    // 업데이트가 사용 가능한 상태 (업데이트 있음) -> 이곳에서 업데이트를 요청해주자
+                    try {
+
+                        mAppUpdateManager.startUpdateFlowForResult(
+                                appUpdateInfo,
+                                // 유연한 업데이트 사용 시 (AppUpdateType.FLEXIBLE) 사용
+                                AppUpdateType.IMMEDIATE,
+                                // 현재 Activity
+                                MainActivity.this,
+                                // 전역변수로 선언해준 Code
+                                MY_REQUEST_CODE);
+
+                    } catch (IntentSender.SendIntentException e) {
+                        Log.e("AppUpdater", "AppUpdateManager Error", e);
+                        e.printStackTrace();
+                    }
+                } else {
+
+                    // 업데이트가 사용 가능하지 않은 상태(업데이트 없음) -> 다음 액티비티로 넘어가도록
+                }
+
+
+            }
+        });// auto update end
+
+
+    }
+    // auto update 앱재실행시
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAppUpdateManager.getAppUpdateInfo().addOnSuccessListener(
+                new OnSuccessListener<AppUpdateInfo>() {
+                    @Override
+                    public void onSuccess(AppUpdateInfo appUpdateInfo) {
+                        if (appUpdateInfo.updateAvailability()
+                                == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                            // 인 앱 업데이트가 이미 실행중이었다면 계속해서 진행하도록
+                            try {
+                                mAppUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, MainActivity.this, MY_REQUEST_CODE);
+                            } catch (IntentSender.SendIntentException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+    }
+    /*
+auto upgrade
+ */
+    // auto update실패시
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+//Log.d("AppUpdate", "Update flow failed! Result code: " + resultCode); // 로그로 코드 확인
+
+                finishAffinity(); // 앱 종료
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/cdhgold/carwash/ui/notifications/MapFrg.java
@@ -0,0 +1,24 @@
+package com.cdhgold.carwash.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.cdhgold.carwash.R;
+
+public class MapFrg extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return root;
+    }
+}
